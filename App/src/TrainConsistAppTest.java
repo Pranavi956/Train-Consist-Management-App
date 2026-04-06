07a2dd585307f3c8e4bb7c1add8a5d9612328948
Pranavi956 <pran0905@gmail.com>
2026-04-06 16:37:50 +0530

    UC15 - Safe cargo assignment using try-catch-finally

--- App/src/TrainConsistAppTest.java
+++ App/src/TrainConsistAppTest.java
@@ -1,60 +1,48 @@
 import org.testng.annotations.Test;
 
-import java.util.*;
 import static org.testng.AssertJUnit.*;
 
 public class TrainConsistAppTest {
 
     @Test
-    public void testException_ValidCapacityCreation() throws InvalidCapacityException {
-        Bogie b = new Bogie("Sleeper", 72);
-        assertEquals(72, b.capacity);
+    public void testCargo_SafeAssignment() {
+        GoodsBogie b = new GoodsBogie("Cylindrical");
+        b.assignCargo("Petroleum");
+
+        assertEquals("Petroleum", b.cargo);
     }
 
     @Test
-    public void testException_NegativeCapacityThrowsException() {
-        try {
-            new Bogie("Sleeper", -10);
-            fail("Exception not thrown");
-        } catch (InvalidCapacityException e) {
-            assertTrue(true);
-        }
+    public void testCargo_UnsafeAssignmentHandled() {
+        GoodsBogie b = new GoodsBogie("Rectangular");
+        b.assignCargo("Petroleum");
+
+        assertNull(b.cargo);
     }
 
     @Test
-    public void testException_ZeroCapacityThrowsException() {
-        try {
-            new Bogie("Sleeper", 0);
-            fail("Exception not thrown");
-        } catch (InvalidCapacityException e) {
-            assertTrue(true);
-        }
+    public void testCargo_CargoNotAssignedAfterFailure() {
+        GoodsBogie b = new GoodsBogie("Rectangular");
+        b.assignCargo("Petroleum");
+
+        assertNull(b.cargo);
     }
 
     @Test
-    public void testException_ExceptionMessageValidation() {
-        try {
-            new Bogie("Sleeper", -5);
-            fail("Exception not thrown");
-        } catch (InvalidCapacityException e) {
-            assertEquals("Capacity must be greater than zero", e.getMessage());
-        }
+    public void testCargo_ProgramContinuesAfterException() {
+        GoodsBogie b = new GoodsBogie("Rectangular");
+        b.assignCargo("Petroleum");
+
+        // Program should continue without crash
+        assertTrue(true);
     }
 
     @Test
-    public void testException_ObjectIntegrityAfterCreation() throws InvalidCapacityException {
-        Bogie b = new Bogie("AC Chair", 54);
-        assertEquals("AC Chair", b.name);
-        assertEquals(54, b.capacity);
-    }
+    public void testCargo_FinallyBlockExecution() {
+        GoodsBogie b = new GoodsBogie("Rectangular");
+        b.assignCargo("Petroleum");
 
-    @Test
-    public void testException_MultipleValidBogiesCreation() throws InvalidCapacityException {
-        List<Bogie> list = Arrays.asList(
-                new Bogie("Sleeper", 72),
-                new Bogie("AC Chair", 54)
-        );
-
-        assertEquals(2, list.size());
+        // If finally runs, program reaches here
+        assertTrue(true);
     }
 }
